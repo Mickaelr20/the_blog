e3b8ca142801e0164899d1c07983672602b2cef1
Rivière Mickael <mickaelr20@gmail.com>
2022-04-04 16:38:27 +0400

    Modification home page

--- src/Template/Pages/index.php
+++ src/Template/Pages/index.php
@@ -91,7 +91,7 @@
 
                 <ul class="timeline">
                     <li>
-                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="/vendors/themes/agency/assets/img/about/1.jpg" alt="..." /></div>
+                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="/img/timeline_formation.jpg" alt="..." /></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
                                 <h4>2020 - 2022</h4>
@@ -119,7 +119,7 @@
                         </div>
                     </li>
                     <li>
-                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="/vendors/themes/agency/assets/img/about/3.jpg" alt="..." /></div>
+                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="/img/timeline_universite.webp" alt="..." /></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
                                 <h4>2016 - 2018</h4>
@@ -134,7 +134,7 @@
                         </div>
                     </li>
                     <li class="timeline-inverted">
-                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="/vendors/themes/agency/assets/img/about/4.jpg" alt="..." /></div>
+                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="/img/timeline_terminale_s.png" alt="..." /></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
                                 <h4>2015 - 2016</h4>
