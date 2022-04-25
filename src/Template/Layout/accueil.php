3aa1f0be46caedf7550c3188bbc3fe71f97bc064
Rivière Mickael <mickaelr20@gmail.com>
2022-04-25 18:29:29 +0400

    Fix: Problèmes avec les Modal de la page d'accueil

--- src/Template/Layout/accueil.php
+++ src/Template/Layout/accueil.php
@@ -34,6 +34,8 @@
     <!-- Core theme JS-->
     <script src="/vendors/themes/agency/js/scripts.js"></script>
     <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
     <?= $pageHelper->printScripts('bottom'); ?>
 </body>
 
